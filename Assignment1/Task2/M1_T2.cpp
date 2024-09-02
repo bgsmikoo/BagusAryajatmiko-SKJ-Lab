f3f9f87c00e925efdc9913a824af925ee345341b
bgsmikoo <mikobgs@gmail.com>
2024-09-03 02:54:22 +0700

    Create M1_T2.cpp

---
+++ Assignment1/Task2/M1_T2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num1 = 13;
+    int num2 = 12;
+    int result = 0;
+
+    result = num1 * num2;
+
+    cout<<"Result: "<<result<<endl;
+
+    return 0;
+}
